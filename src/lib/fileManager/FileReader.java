7b3c2a9acfb5af635d9339f4dd3a1240120e1a7f
Cristan Serrano <nanonathan.123@gmail.com>
2024-03-06 23:06:02 -0300

    creo clase FileReader

--- src/lib/fileManager/FileReader.java
+++ src/lib/fileManager/FileReader.java
@@ -1,5 +1,44 @@
 package src.lib.fileManager;
 
+import java.io.BufferedReader;
+import java.io.File;
+
 public class FileReader {
-    
+    private BufferedReader bufferedReader;
+
+    public FileReader(String path){
+        File file = new File(path);
+
+        //Valida que el archivo exista y no sea un direcctorio
+        if(file.exists() && file.isFile()){
+            try {
+                //Crea el bufferedReader
+                bufferedReader = new BufferedReader(new java.io.FileReader(file));
+            }
+            catch (Exception e) {
+                System.out.println("ERROR: No se ha podido crear el lector de archivo.");
+            }
+        }
+        else {
+            System.out.println("ERROR: El archivo no existe o es un directorio. Se busca en: " + file.getAbsolutePath());
+        }
+    }
+
+    public String getLine () {
+        String line = null;
+        //Valida que se haya creado el bufferedReader
+        if(bufferedReader != null){
+            try {
+                line = bufferedReader.readLine();
+                //Cierra el reader si termina de leer
+                if(line == null){
+                    bufferedReader.close();
+                }
+            }
+            catch (Exception e) {
+                System.out.println("ERROR: No se ha podido leer la siguiente linea del archivo.");
+            }
+        }
+        return line;
+    }
 }
