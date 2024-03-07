5a62fdd4a7cc1836056ae0ed56f544bcc06d05d1
Cristan Serrano <nanonathan.123@gmail.com>
2024-03-07 15:29:11 -0300

    implemento printer

--- src/lib/Printer.java
+++ src/lib/Printer.java
@@ -1,5 +1,21 @@
 package src.lib;
 
+import src.lib.tokenHelper.Token;
+
 public class Printer {
-    
+
+    public static  void showTokens (Token tokens []) {
+        System.out.println("CORRECTO: ANALISIS LEXICO");
+        System.out.println("| TOKEN | LEXEMA | NUMERO DE LINEA (NUMERO DE COLUMNA) |");
+
+        for (Token token : tokens) {
+            System.out.println(token.toString());
+        }
+    }
+
+    public static void showErrors (Integer line, Integer column, String descripcion, String type) {
+        System.out.println("ERROR: " + type);
+        System.out.println("| NUMERO DE LINEA: | NUMERO DE COLUMNA: | DESCRIPCION: |");
+        System.out.println("| LINEA " + line.toString() + " | COLUMNA " + column.toString() + " | " + descripcion + " |");
+    }
 }
