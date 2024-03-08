fafbed89f4d81583ace4c99680f784d9836091d1
Nano-Nathan <nanonathan.123@gmail.com>
2024-03-08 12:16:52 -0300

    documentación al refactor

--- src/lib/FileManager.java
+++ src/lib/FileManager.java
@@ -3,9 +3,28 @@ package src.lib;
 import java.io.BufferedReader;
 import java.io.File;
 
+/**
+ * Esta clase se utilizará para leer línea por línea el código fuente de los
+ * archivos .ru
+ * 
+ * @author Cristian Serrano
+ * @author Federico Gimenez
+ * @since 06/03/2024
+ */
 public class FileManager {
     private BufferedReader bufferedReader;
 
+    /**
+     * Constructor de la clase. 
+     * 
+     * <br/>RECORDAR QUE EL PATH COMIENZA CON LA RUTA DONDE NOS ENCONTRAMOS EN LA
+     * TERMINAL AL MOMENTO DE EJECUTAR EL PROGRAMA.
+     * 
+     * <br/>Por ejemplo: C:\\user\\code\\
+     * 
+     * @since 06/03/2024
+     * @param path Path hacia el fichero que se desea importar como código fuente.
+     */
     public FileManager(String path){
         File file = new File(path);
 
@@ -24,6 +43,12 @@ public class FileManager {
         }
     }
 
+    /**
+     * Método que lee y retorna la siguiente línea del documento.
+     * 
+     * @since 06/03/2024
+     * @return Siguiente línea del documento.
+     */
     public String getLine () {
         String line = null;
         //Valida que se haya creado el bufferedReader
--- src/lib/tokenHelper/IDToken.java
+++ src/lib/tokenHelper/IDToken.java
@@ -1,99 +1,107 @@
 package src.lib.tokenHelper;
 
+/**
+ * Un enum que contendra todos los tipos de tokens que pueden existir dentro
+ * del código fuente del un proyecto tinyRu.
+ * 
+ * @author Cristian Serrano
+ * @author Federico Gimenez
+ * @since 06/03/2024
+ */
 public enum IDToken {
     //Constructor de clase
-    CONSTRUCTOR,
+    /** Para el nombre del constructor de la clase */ CONSTRUCTOR,
     //Comentario
-    COMMENT, // /?
+    /** /? */ COMMENT,
     //Operador de asignacion =
-    ASSIGN,
+    /** = */ ASSIGN,
 
     //Structuras predefinidas
-    spOBJECT,
-    spIO,
+    /** Struct predefinida Object */ spOBJECT,
+    /** Struct predefinida IO */ spIO,
     
     //Tipos de datos
-    typeINT,
-    typeSTR,
-    typeBOOL,
-    typeCHAR,
-    typeCLASS,
+    /** Tipo de dato INT */ typeINT,
+    /** Tipo de dato STR */ typeSTR,
+    /** Tipo de dato BOOL */ typeBOOL,
+    /** Tipo de dato CHAR */ typeCHAR,
+    /** Para cualquier otro tipo de dato definido en una clase */ typeCLASS,
     
     
     //Para valores constantes
-    constINT,
-    constSTR,
-    constBOOL,
-    constCHAR,
+    /** Para constantes de tipo INT */ constINT,
+    /** Para constantes de tipo STR */ constSTR,
+    /** Para constantes de tipo BOOL */ constBOOL,
+    /** Para constantes de tipo CHAR */ constCHAR,
 
 
     //Operadores aritmeticos
-    oSUM, // +
-    oSUB, // -
-    oMULT, // *
-    oSUM_SUM, // ++
-    oSUB_SUB, // --
-    oDIV, // /
-    oMOD, // %
+    /** + */ oSUM,
+    /** - */ oSUB,
+    /** <p>\u002A</p> */ oMULT,
+    /** ++ */ oSUM_SUM,
+    /** -- */ oSUB_SUB,
+    /** / */ oDIV,
+    /** % */ oMOD,
 
 
     //Operadores logicos
-    oAND,
-    oOR,
-    oNOT,
-    oMIN,
-    oMIN_EQ,
-    oEQUAL,
-    oMAX,
-    oMAX_EQ,
-    oNOT_EQ,
+    /** &amp;&amp; */ oAND,
+    /** || */ oOR,
+    /** ! */ oNOT,
+    /** &lt; */ oMIN,
+    /** &le; */ oMIN_EQ,
+    /** == */ oEQUAL,
+    /** > */ oMAX,
+    /** &ge; */ oMAX_EQ,
+    /** != */ oNOT_EQ,
 
 
     //Simbolos especiales
-    sPAR_OPEN, // (
-    sPAR_CLOSE, // )
-    sCOR_OPEN, // [
-    sCOR_CLOSE, // ]
-    sKEY_OPEN, // {
-    sKEY_CLOSE, // }
-    sDOT, // .
-    sCOM, // ,
-    sCOLON, // :
-    sSEMICOLON, // ;
-    sARROW_METHOD, // ->
+    /** ( */ sPAR_OPEN,
+    /** ) */ sPAR_CLOSE,
+    /** [ */ sCOR_OPEN,
+    /** ] */ sCOR_CLOSE,
+    /** { */ sKEY_OPEN,
+    /** } */ sKEY_CLOSE,
+    /** . */ sDOT,
+    /** , */ sCOM,
+    /** : */ sCOLON,
+    /** ; */ sSEMICOLON,
+    /** -> */ sARROW_METHOD,
     
 
     //Identificadores
-    idSTRUCT,
-    idVAR,
-    idMETHOD,
+    /** Para identificadores de estructuras*/ idSTRUCT,
+    /** Para identificadores de variables*/ idVAR,
+    /** Para identificadores de métodos*/ idMETHOD,
 
 
     //Texto plano
-    tBLANK_SPACE,
-    tNEW_LINE,
-    tRETURN,
-    tTAB,
-    tVERTICAL_TAB,
+    /** Espacio en blanco */ tBLANK_SPACE,
+    /** \n */ tNEW_LINE,
+    /** \r */ tRETURN,
+    /** \t */ tTAB,
+    /** \v */ tVERTICAL_TAB,
     
 
     //Palabras clave o reservadas
-    pSTRUCT,
-    pIMPL,
-    pWHILE,
-    pIF,
-    pELSE,
-    pFALSE,
-    pTRUE,
-    pNIL,
-    pNEW,
-    pFN,
-    pST,
-    pPRI,
-    pPUB,
-    pSELF,
-    pVOID,
-    pSTART,
-    pARRAY,
-    pRET;
+    /** Para palabra reservada STRUCT */ pSTRUCT,
+    /** Para palabra reservada IMPL */ pIMPL,
+    /** Para palabra reservada WHILE */ pWHILE,
+    /** Para palabra reservada IF */ pIF,
+    /** Para palabra reservada ELSE */ pELSE,
+    /** Para palabra reservada FALSE */ pFALSE,
+    /** Para palabra reservada TRUE */ pTRUE,
+    /** Para palabra reservada NIL */ pNIL,
+    /** Para palabra reservada NEW */ pNEW,
+    /** Para palabra reservada FN */ pFN,
+    /** Para palabra reservada ST */ pST,
+    /** Para palabra reservada PRI */ pPRI,
+    /** Para palabra reservada PUB */ pPUB,
+    /** Para palabra reservada SELF */ pSELF,
+    /** Para palabra reservada VOID */ pVOID,
+    /** Para palabra reservada START */ pSTART,
+    /** Para palabra reservada ARRAY */ pARRAY,
+    /** Para palabra reservada RET */ pRET;
 }
