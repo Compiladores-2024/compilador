a1685e3950e1ace91e73e02f50d537446446564f
Nano-Nathan <nanonathan.123@gmail.com>
2024-04-19 14:22:02 -0300

    agrego métodos a la tabla de símbolos

--- src/lib/semanticHelper/SymbolTable.java
+++ src/lib/semanticHelper/SymbolTable.java
@@ -16,4 +16,49 @@ public class SymbolTable {
     Method currentMethod;
     HashMap<String, Struct> structs;
 
+    /**
+     * Método que agrega una estructura a la tabla de símbolos.
+     * 
+     * @since 19/04/2024
+     */
+    public void addStruct() {
+
+    }
+
+    /**
+     * Método que agrega una variable a la tabla de símbolos.
+     * 
+     * @since 19/04/2024
+     */
+    public void addVar() {
+
+    }
+
+    /**
+     * Método que agrega un método a la tabla de símbolos.
+     * 
+     * @since 19/04/2024
+     */
+    public void addMethod() {
+
+    }
+
+    /**
+     * Método que agrega un parámetro a la tabla de símbolos.
+     * 
+     * @since 19/04/2024
+     */
+    public void addParam() {
+
+    }
+
+    /**
+     * Reescritura del método, convierte los datos en JSON.
+     * 
+     * @since 19/04/2024
+     * @return Estructura de datos en formato JSON
+     */
+    public String toJSON() {
+        return "";
+    }
 }
