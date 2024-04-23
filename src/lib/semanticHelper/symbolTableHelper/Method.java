0c7a151b720abc7032b58db3e0778651cc93e6db
Nano-Nathan <nanonathan.123@gmail.com>
2024-04-22 23:31:01 -0300

    actualizo generacion de signature

--- src/lib/semanticHelper/symbolTableHelper/Method.java
+++ src/lib/semanticHelper/symbolTableHelper/Method.java
@@ -20,7 +20,6 @@ public class Method extends Metadata{
     private int currentVarIndex;
     private HashMap<String, Param> params;
     private HashMap<String, Variable> variables;
-    private String[] orderParams;
 
     /**
      * Constructor de la clase.
@@ -62,12 +61,12 @@ public class Method extends Metadata{
         String sParams = "";
 
         //Genera el string de parametros
-        for (String paramName : orderParams) {
+        for (String paramName : order(params)) {
             sParams += params.get(paramName).toString() + " ";
         }
 
         //Retorna la signature
-        return (this.isStatic ? "st" : "") + getName() + " " + sParams + "-> " + returnType.toString();
+        return (this.isStatic ? "st " : "") + getName() + " " + sParams + "-> " + returnType.toString();
     }
     
     /**
