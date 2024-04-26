86d9f7089a710c90dbf98327d5b046d1d2a5fd6f
Nano-Nathan <nanonathan.123@gmail.com>
2024-04-26 20:41:59 -0300

    modifico toString param

--- src/lib/semanticHelper/symbolTableHelper/Param.java
+++ src/lib/semanticHelper/symbolTableHelper/Param.java
@@ -31,7 +31,7 @@ public class Param extends Metadata{
 
 
     public String toString() {
-        return type.toString() + " " + getName();
+        return type.getLexema() + " " + getName();
     }
     
     /**
