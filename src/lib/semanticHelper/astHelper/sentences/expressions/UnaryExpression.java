ef94ba67ec61d8068342b91349d457549ec9d61f
Nano-Nathan <nanonathan.123@gmail.com>
2024-05-10 17:02:11 -0300

    init operadores

--- src/lib/semanticHelper/astHelper/sentences/expressions/UnaryExpression.java
+++ src/lib/semanticHelper/astHelper/sentences/expressions/UnaryExpression.java
@@ -1,9 +1,14 @@
 package src.lib.semanticHelper.astHelper.sentences.expressions;
 
-import src.lib.tokenHelper.Token;
+import src.lib.tokenHelper.IDToken;
 
 public class UnaryExpression extends Expression{
     
     private Expression expression;
-    private Token operator;
+    private IDToken operator;
+
+    public UnaryExpression (IDToken operator, Expression expression) {
+        this.operator = operator;
+        this.expression = expression;
+    }
 }
